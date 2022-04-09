1d75180ea1724b9acdd4e9c1ca6b4806c12a2cab
Natalia <nat.kaseeva@gmail.com>
2022-04-08 19:55:02 -0600

    Database with mongoDB and mongoose was added

--- app.js
+++ app.js
@@ -3,13 +3,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-let posts = [];
 
 const app = express();
 
@@ -18,60 +16,69 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser: true});
 
-app.get('/', function(req, res) {
-  res.render('home', {
-    startingContent: homeStartingContent,
-    posts: posts
+//create a new postSchema that contains a title and content
+const postSchema = {
+  title: String,
+  content: String
+};
+
+//create a new mongoose model using the schema to define your posts collection
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  //find all the posts in the posts collection and render that in the home.ejs file
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
   });
 });
 
-app.get('/about', function(req, res) {
-  res.render('about', {aboutContent: aboutContent});
+app.get("/compose", function(req, res){
+  res.render("compose");
 });
 
-app.get('/contact', function(req, res) {
-  res.render('contact', {contactContent: contactContent });
-});
+app.post("/compose", function(req, res){
 
-app.get('/compose', function(req, res) {
-  res.render('compose');
-});
-
-app.post('/compose', function(req, res) {
-
-  const post = {
+  //create a new post document using your mongoose model
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
+  });
 
-  posts.push(post);
-
-  res.redirect('/');
-});
-
-app.get('/posts/:postName', function(req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title); 
-
-    if (requestedTitle === storedTitle) {
-      //console.log('Math found!');
-      res.render('post', {
-        title: post.title,
-        content: post.content
-      });
+  //add a call back to the mongoose save() method.
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
     }
   });
 });
 
+app.get("/posts/:postId", function(req, res){
 
+const requestedPostId = req.params.postId;
 
+  //use the findOne() method to find the post with a matching id in the posts collection
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
 
+});
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
 
-
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
 
 app.listen(3000, function() {
